1b1ac6991b082bd0492d46bbff084269e8427f8e
hgenth <hgenth@haverford.edu>
2022-12-14 16:58:23 -0500

    j need hangman

--- Game.py
+++ Game.py
@@ -5,37 +5,106 @@ from VLibs import Mam
 from VLibs import FF
 from VLibs import Moods
 from VLibs import Shoes
-#These are the themes
+from VLibs import Dict
+global word
+
+
+
+print("Hello, Thank you for playing  Harry's Hangman game\n") # welcome outside of path function so it is not re-printed
+name = input("What is your name?: ")
+print("Hi", name, "Good luck\n")
+
 def path():
-    choice = list
-    option = input("Choose a theme: (1)English Dictionary, (2) MLB Teams (3)US Presidents, (4)Mammals, (5)Fast Food Restaurants, (5)Moods, (6)Shoe Brands, (7)English Dictionary   ")
+    option = input("Choose a theme: (1)English Dictionary, (2) MLB Teams (3)US Presidents, (4)Mammals, (5)Fast Food Restaurants, (6)Moods, (7)Shoe Brands. Type 1, 2, 3, 4, 5, 6, or 7")
     if option == "1":
         print("You've selected English Dictionary. Note that each answer is only one word and all lowercase")
+        list = Dict
     elif option == "2":
-        print("Youv'e selected MLB Teams. Note that each answer is simply the mascot name (Orioles, Yankees, Redsox), are one word and all lowercase")
-        choice = MLB
+        print("Youv'e selected MLB Teams. Note that each answer is simply the mascot name (orioles, yankees, redsox), are one word and all lowercase")
+        list = MLB
     elif option == "3":
         print("You've selected US Presidents. Note that each answer is only the presidents last name (vanburen, bush, mckinley), and all letters are lowercase.")
-        choice = Pres
+        list = Pres
     elif option == "4":
         print("You've selected Mammals. Note that each answer is only one word and all letters are lowercase (bear, donkey, horse)")
-        choice = Mam
+        list = Mam
     elif option == "5":
-        print("You've selected Fast Food restaurants. Note that each answer is only one word and all lowercase (kfc, tacobell, mcdonalds, chickfila ")
-        choice = FF
+        print("You've selected Fast Food restaurants. Note that each answer is only one word and all lowercase (kfc, tacobell, mcdonalds, chickfila)")
+        list = FF
     elif option == "6":
-        print("You've selected Moods. Note that each mood is one word with all lowercase letters (")
-        choice = Moods
+        print("You've selected Moods. Note that each mood is one word with all lowercase letters (happy, sad)")
+        list = Moods
     elif option == "7":
         print("You've selected Shoes. Note that each answer is one word and only has lowercase letter (underarmour, nike, adidas)")
-        choice = Shoes
-
+        list = Shoes
     else:
-        print("invalid input")
+        print("invalid input, please type either 1, 2, 3, 4, 5, 6, or 7 based on what theme you'd like to choose")
         return path()
-    print(choice)
+    word = random.choice(list)
+    print(word)
+    return hang
+
+def start_q():
+        play = input("Would you like to play another time?, yes = yes no = no")
+        if play == "yes":
+            return path()
+        elif play == "no":
+            print("Thank you very much for playing")
+        else:
+            print("Invalid input, please type yes or no if you'd like to play again")
+            return start_q()
+
+def hang():
+    numb = 0
+    length = len(word)
+    ill = "_" * length
+    prev_guess = []
+    play = ""
+    lim = 5 # limit to number of guesses, limbs added each time
+    guess = input("The word is: " + ill + "Please type a letter to guess: ")
+    if len(guess) == 0 or len(guess) >= 2 or guess <= "9": # this makes sure the guess is a letter
+        print("Invalid input")
+        hang()
+    elif guess in word:
+        prev_guess.extend[guess]
+        index = word.find(guess)
+        word = word[index] = "_" + word[index + 1]
+        ill = ill + word.find(index) + 1
+        print(ill)
+
+    elif guess in prev_guess:
+        print("Use a different letter")
+#following code is for differing responses based on previous incorrect guesses
+    else:
+        numb += 1
+        if numb == 1:
+            print("Wrong guess. " + str(lim - numb) + "guesses left")
+            return hang
+        elif numb == 2:
+            print("Wrong guess. " + str(lim - numb) + "guesses left")
+            return hang
+        elif numb == 3:
+            print("Wrong guess. " + str(lim - numb) + "guesses left")
+            return hang
+        elif numb == 4:
+            print("Wrong guess. " + str(lim - numb) + "guesses left")
+            return hang
+        elif numb == 5: #user loses the game
+            print("Wrong guess. You lose!")
+            print("The word was", word)
+            return start_q
+    if word == "_" * length:
+            print("Well done, youve guessd the word")
+            return start_q()
+    elif numb != lim:
+            hang()
+
+#path is the UI used to select the theme and give basic instructions regarding the game
 
 path()
+
+
+
 
 
 
--- Main.py
+++ Main.py
@@ -1,61 +1,22 @@
-#will eventually add a random word compeont for a 5 word sorted dicitonary, for this case we are just going to use a given single word
-from english_words import english_words_set
-from VLibs import MLB
-from VLibs import Pres
-global lib
-global theme
-from Game import t
+import random
+from Game import path
+from Game import start_q
+from Game import choice
 
-print("Hello, Thank you for playing  Harry's Hangman game\n")
-name = input("What is your name?: ")
-print("Hi", name, "Good luck")
-path = input("Would you like to play with random words, or choose a theme? Please type r for random and t for theme: ")
-    if path == "r" or "R":
-        lib = english_words_set
-    elif path == "t" or "T":
-        print("Please choose from one of the following themes: MLB Baseball teams (MLB), US Presidents (US), Fast-Food Chains (FF), Famous Poets (FP), Popular Brands (PB)"))
-        theme = input("Please select your choice by typing MLB, US, FF, FP,or PB based on the above options: ")
-            if theme == "MLB":
-                print("The generated MLB team will be the mascot ie: Orioles, Redsox, Whitesox, Cardinals etc. The hometown is not included and all answers are oneword with the first letter capitalized")
-                lib = MLB
-                return
-            elif theme == "Pres":
-                print("The generated Hangman word will be ")
 
-def Main():
-    global numb #naming all global vars and inputting each
-    global length
-    global word
-    global prev_guess
-    global play
-    global ill
-    word = random.randint(0, len(lib) - 1)
-    numb = 0
-    length = len(word)
-    ill= "_" * length
-    prev_guess = []
-    play = ""
 
-def start_q():
-    global play
-    play = input("Would you like to play another time?, yes = yes no = no")
-    while play not in ["yes", "no", "Yes", "No"]:
-        play = input("Would you like to play another time?, yes = yes no = no")
-        if play == "yes" or "Yes":
-            return Main()
-        elif play == "no" or "No":
-            print("Thank you very much for playing")
 
 def hang():
+    path()
     global numb
     global length
-    global word
+
     global prev_guess
     global play
-    global ill
-    word = "Chair"
+
+
     numb = 0
-    length = len(word)
+    length = len(random.choice(choice))
     ill = "_" * length
     prev_guess = []
     play = ""
@@ -92,15 +53,14 @@ def hang():
         elif numb == 5: #user loses the game
             print("Wrong guess. You lose!")
             print("The word was", word)
-            start_q()
+            return start_q
     if word == "_" * length:
             print("Well done, youve guessd the word")
-            start_q()
+            return start_q()
     elif numb != lim:
             hang()
-Main()
-
 hang()
+
 
 
 
